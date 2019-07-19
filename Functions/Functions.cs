e79805f7ca645573c6c23585d04a1e10e48f1321
Julien <Lafille.Julien@gmail.com>
2019-07-19 15:57:58 +0200

    sould work

--- Functions/Functions.cs
+++ Functions/Functions.cs
@@ -5,6 +5,11 @@ namespace Functions
     public class Functions{
 
         public static double sin(double d){
+
+            if(d == 0.0){
+                return 0.0;
+            }
+
             throw new NotImplementedException();
         }
 
